passepartoutvpn/passepartout

commit 87527ed290af299064e0216bdf229ab0b6c389b3

Upgrade to TunnelKit 2.0
Major reorganization. No changes in logic.

Rename by_* to pod_*.

Passepartout/Sources/Model/ConnectionService.swift
@@ -89,7 +89,7 @@ public class ConnectionService: Codable {
 
     private let keychain: Keychain
     
-    public var baseConfiguration: TunnelKitProvider.Configuration
+    public var baseConfiguration: OpenVPNTunnelProvider.Configuration
     
     private var cache: [ProfileKey: ConnectionProfile]
     
@@ -124,7 +124,7 @@ public class ConnectionService: Codable {
     
     public weak var delegate: ConnectionServiceDelegate?
     
-    public init(withAppGroup appGroup: String, baseConfiguration: TunnelKitProvider.Configuration) {
+    public init(withAppGroup appGroup: String, baseConfiguration: OpenVPNTunnelProvider.Configuration) {
         guard let defaults = UserDefaults(suiteName: appGroup) else {
             fatalError("No entitlements for group '\(appGroup)'")
         }
@@ -157,7 +157,7 @@ public class ConnectionService: Codable {
         self.defaults = defaults
         keychain = Keychain(group: appGroup)
 
-        baseConfiguration = try container.decode(TunnelKitProvider.Configuration.self, forKey: .baseConfiguration)
+        baseConfiguration = try container.decode(OpenVPNTunnelProvider.Configuration.self, forKey: .baseConfiguration)
         activeProfileKey = try container.decodeIfPresent(ProfileKey.self, forKey: .activeProfileKey)
         preferences = try container.decode(EditablePreferences.self, forKey: .preferences)
 
@@ -281,20 +281,10 @@ public class ConnectionService: Codable {
                     profile = providerProfile
                     
                 case .host:
-                    let hostProfile = try decoder.decode(HostConnectionProfile.self, from: data)
-
-                    // XXX: migrate old endpointProtocols
-                    if hostProfile.parameters.sessionConfiguration.endpointProtocols == nil {
-                        var sessionBuilder = hostProfile.parameters.sessionConfiguration.builder()
-                        sessionBuilder.endpointProtocols = hostProfile.parameters.endpointProtocols
-                        var parametersBuilder = hostProfile.parameters.builder()
-                        parametersBuilder.sessionConfiguration = sessionBuilder.build()
-                        hostProfile.parameters = parametersBuilder.build()
-                    }
-                    
-                    // XXX: re-read routing policies for 
-
-                    profile = hostProfile
+//                    let hostProfile = try decoder.decode(HostConnectionProfile.self, from: data)
+//
+//                    profile = hostProfile
+                    break
                 }
                 cache[key] = profile
             } catch let e {
@@ -350,8 +340,8 @@ public class ConnectionService: Codable {
             }
 
             // can fail due to passphrase (migration is non-interactive)
-            if let result = try? ConfigurationParser.parsed(fromURL: url) {
-                host.parameters = TunnelKitProvider.ConfigurationBuilder(sessionConfiguration: result.configuration).build()
+            if let result = try? OpenVPN.ConfigurationParser.parsed(fromURL: url) {
+                host.parameters = OpenVPNTunnelProvider.ConfigurationBuilder(sessionConfiguration: result.configuration).build()
             } else {
 
                 // fall back to the safer option
@@ -602,7 +592,7 @@ public class ConnectionService: Codable {
         try? FileManager.default.removeItem(at: url)
     }
 
-    public var vpnLastError: TunnelKitProvider.ProviderError? {
+    public var vpnLastError: OpenVPNTunnelProvider.ProviderError? {
         return baseConfiguration.lastError(in: appGroup)
     }
     

Passepartout-iOS-Tunnel/PacketTunnelProvider.swift
@@ -25,7 +25,7 @@
 
 import TunnelKit
 
-class PacketTunnelProvider: TunnelKitProvider {
+class PacketTunnelProvider: OpenVPNTunnelProvider {
     override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
         appVersion = "\(GroupConstants.App.name) \(GroupConstants.App.versionString)"
         dnsTimeout = GroupConstants.VPN.dnsTimeout

Passepartout-iOS/AppDelegate.swift
@@ -106,11 +106,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         let passphraseCancelBlock = {
             _ = try? FileManager.default.removeItem(at: url)
         }
-        guard let parsingResult = ConfigurationParser.Result.from(url, withErrorAlertIn: target, passphrase: passphrase, passphraseBlock: passphraseBlock, passphraseCancelBlock: passphraseCancelBlock) else {
+        guard let parsingResult = OpenVPN.ConfigurationParser.Result.from(url, withErrorAlertIn: target, passphrase: passphrase, passphraseBlock: passphraseBlock, passphraseCancelBlock: passphraseCancelBlock) else {
             return true
         }
         if let warning = parsingResult.warning {
-            ConfigurationParser.Result.alertImportWarning(url: url, in: target, withWarning: warning) {
+            OpenVPN.ConfigurationParser.Result.alertImportWarning(url: url, in: target, withWarning: warning) {
                 if $0 {
                     self.handleParsingResult(parsingResult, in: target)
                 } else {
@@ -123,7 +123,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         return true
     }
     
-    private func handleParsingResult(_ parsingResult: ConfigurationParser.Result, in target: UIViewController) {
+    private func handleParsingResult(_ parsingResult: OpenVPN.ConfigurationParser.Result, in target: UIViewController) {
 
         // already presented: update parsed configuration
         if let nav = target as? UINavigationController, let wizard = nav.topViewController as? WizardHostViewController {

Passepartout-iOS/Global/ConfigurationParserResult+Alerts.swift
@@ -1,5 +1,5 @@
 //
-//  ConfigurationParserResult+Alerts.swift
+//  OpenVPN.ConfigurationParserResult+Alerts.swift
 //  Passepartout-iOS
 //
 //  Created by Davide De Rosa on 10/27/18.
@@ -31,16 +31,16 @@ import Passepartout_Core
 
 private let log = SwiftyBeaver.self
 
-extension ConfigurationParser.Result {
+extension OpenVPN.ConfigurationParser.Result {
     static func from(_ url: URL, withErrorAlertIn viewController: UIViewController, passphrase: String?,
-                     passphraseBlock: @escaping (String) -> Void, passphraseCancelBlock: (() -> Void)?) -> ConfigurationParser.Result? {
+                     passphraseBlock: @escaping (String) -> Void, passphraseCancelBlock: (() -> Void)?) -> OpenVPN.ConfigurationParser.Result? {
 
-        let result: ConfigurationParser.Result
+        let result: OpenVPN.ConfigurationParser.Result
         let fm = FileManager.default
 
         log.debug("Parsing configuration URL: \(url)")
         do {
-            result = try ConfigurationParser.parsed(fromURL: url, passphrase: passphrase)
+            result = try OpenVPN.ConfigurationParser.parsed(fromURL: url, passphrase: passphrase)
         } catch let e as ConfigurationError {
             switch e {
             case .encryptionPassphrase, .unableToDecrypt(_):

Passepartout-iOS/Global/IssueReporter.swift
@@ -92,7 +92,7 @@ class IssueReporter: NSObject {
         }
         if let url = configurationURL {
             do {
-                let parsedFile = try ConfigurationParser.parsed(fromURL: url, returnsStripped: true)
+                let parsedFile = try OpenVPN.ConfigurationParser.parsed(fromURL: url, returnsStripped: true)
                 if let attachment = parsedFile.strippedLines?.joined(separator: "\n").data(using: .utf8) {
                     vc.addAttachmentData(attachment, mimeType: AppConstants.IssueReporter.MIME.configuration, fileName: AppConstants.IssueReporter.Filenames.configuration)
                 }

Passepartout-iOS/Global/Theme+Cells.swift
@@ -75,7 +75,7 @@ extension SettingTableViewCell {
         accessoryType = .none
     }
     
-    func applyVPN(_ theme: Theme, with vpnStatus: VPNStatus?, error: TunnelKitProvider.ProviderError?) {
+    func applyVPN(_ theme: Theme, with vpnStatus: VPNStatus?, error: OpenVPNTunnelProvider.ProviderError?) {
         leftTextColor = theme.palette.primaryText
         guard let vpnStatus = vpnStatus else {
             rightText = L10n.Vpn.disabled

Passepartout-iOS/Scenes/ConfigurationModificationDelegate.swift
@@ -27,7 +27,7 @@ import Foundation
 import TunnelKit
 
 protocol ConfigurationModificationDelegate: class {
-    func configuration(didUpdate newConfiguration: SessionProxy.Configuration)
+    func configuration(didUpdate newConfiguration: OpenVPN.Configuration)
     
     func configurationShouldReinstall()
 }

Passepartout-iOS/Scenes/ConfigurationViewController.swift
@@ -35,9 +35,9 @@ class ConfigurationViewController: UIViewController, TableModelHost {
     
     private lazy var itemRefresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refresh))
 
-    var initialConfiguration: SessionProxy.Configuration!
+    var initialConfiguration: OpenVPN.Configuration!
     
-    private lazy var configuration: SessionProxy.ConfigurationBuilder = initialConfiguration.builder()
+    private lazy var configuration: OpenVPN.ConfigurationBuilder = initialConfiguration.builder()
     
     var originalConfigurationURL: URL?
 
@@ -138,9 +138,9 @@ class ConfigurationViewController: UIViewController, TableModelHost {
             log.warning("Resetting with no original configuration set? Bad table model?")
             return
         }
-        let parsingResult: ConfigurationParser.Result
+        let parsingResult: OpenVPN.ConfigurationParser.Result
         do {
-            parsingResult = try ConfigurationParser.parsed(fromURL: originalURL)
+            parsingResult = try OpenVPN.ConfigurationParser.parsed(fromURL: originalURL)
         } catch let e {
             log.error("Could not parse original configuration: \(e)")
             return
@@ -373,7 +373,7 @@ extension ConfigurationViewController: UITableViewDataSource, UITableViewDelegat
         
         switch model.row(at: indexPath) {
         case .cipher:
-            let vc = OptionViewController<SessionProxy.Cipher>()
+            let vc = OptionViewController<OpenVPN.Cipher>()
             vc.title = settingCell?.leftText
             vc.options = [.aes128cbc, .aes192cbc, .aes256cbc, .aes128gcm, .aes192gcm, .aes256gcm]
             vc.selectedOption = configuration.cipher
@@ -385,7 +385,7 @@ extension ConfigurationViewController: UITableViewDataSource, UITableViewDelegat
             navigationController?.pushViewController(vc, animated: true)
             
         case .digest:
-            let vc = OptionViewController<SessionProxy.Digest>()
+            let vc = OptionViewController<OpenVPN.Digest>()
             vc.title = settingCell?.leftText
             vc.options = [.sha1, .sha224, .sha256, .sha384, .sha512]
             vc.selectedOption = configuration.digest
@@ -397,7 +397,7 @@ extension ConfigurationViewController: UITableViewDataSource, UITableViewDelegat
             navigationController?.pushViewController(vc, animated: true)
 
         case .compressionFraming:
-            let vc = OptionViewController<SessionProxy.CompressionFraming>()
+            let vc = OptionViewController<OpenVPN.CompressionFraming>()
             vc.title = settingCell?.leftText
             vc.options = [.disabled, .compLZO, .compress]
             vc.selectedOption = configuration.compressionFraming ?? .disabled
@@ -416,7 +416,7 @@ extension ConfigurationViewController: UITableViewDataSource, UITableViewDelegat
                 return
             }
             
-            let vc = OptionViewController<SessionProxy.CompressionAlgorithm>()
+            let vc = OptionViewController<OpenVPN.CompressionAlgorithm>()
             vc.title = settingCell?.leftText
             vc.options = [.disabled, .LZO]
             vc.selectedOption = configuration.compressionAlgorithm ?? .disabled
@@ -449,7 +449,7 @@ extension ConfigurationViewController: UITableViewDataSource, UITableViewDelegat
 
 // MARK: -
 
-private extension SessionProxy.CompressionFraming {
+private extension OpenVPN.CompressionFraming {
     var cellDescription: String {
         let V = L10n.Configuration.Cells.self
         switch self {
@@ -465,7 +465,7 @@ private extension SessionProxy.CompressionFraming {
     }
 }
 
-private extension SessionProxy.CompressionAlgorithm {
+private extension OpenVPN.CompressionAlgorithm {
     var cellDescription: String {
         let V = L10n.Configuration.Cells.self
         switch self {

luomoxu/passepartout-core-apple

commit 5dc0bce4108c43c2c773427f49469fa253426917

Fix regression in host profiles deserialization
Caused crash on launch in passepartout-ios (1931).

Broken in: https://github.com/passepartoutvpn/passepartout-ios/commit/87527ed290af299064e0216bdf229ab0b6c389b3#diff-19758756c410a7f91efde507d127a8ccR284

Surely commented for testing, then forgotten to uncomment.

Passepartout/Sources/Model/ConnectionService.swift
@@ -281,10 +281,9 @@ public class ConnectionService: Codable {
                     profile = providerProfile
                     
                 case .host:
-//                    let hostProfile = try decoder.decode(HostConnectionProfile.self, from: data)
-//
-//                    profile = hostProfile
-                    break
+                    let hostProfile = try decoder.decode(HostConnectionProfile.self, from: data)
+
+                    profile = hostProfile
                 }
                 cache[key] = profile
             } catch let e {
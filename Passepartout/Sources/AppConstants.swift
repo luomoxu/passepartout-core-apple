d30b99b57f896f3a77ac7e5fb833405c5d386664
Davide De Rosa <keeshux@gmail.com>
2019-09-05 17:55:52 +0200

    Move rating count somewhat back to Core constants

--- Passepartout/Sources/AppConstants.swift
+++ Passepartout/Sources/AppConstants.swift
@@ -348,4 +348,14 @@ public class AppConstants {
             )
         ]
     }
+
+    public struct Rating {
+        public static let eventCount: Int = {
+            #if os(iOS)
+            return 3
+            #else
+            return 10
+            #endif
+        }()
+    }
 }
